e91456be276fd7dff8263c6e2bb5d30807672aea
Aryan Misra <aryanmisra4@gmail.com>
2020-04-14 18:22:51 -0400

    emailer new update

--- emailer_new.py
+++ emailer_new.py
@@ -9,10 +9,7 @@ import base64
 import re
 import json
 from time import sleep
-spreadsheet_body = {
-    'properties': {
-        'title': 'Your Email Chain'
-    }}
+
 
 def callback(request_id, response, exception):
     if exception:
@@ -20,12 +17,15 @@ def callback(request_id, response, exception):
         print (exception)
     else:
         print ("Permission Id: %s" % response.get('id'))
+
 def write_json(data, filename='sheetslink.json'): 
     with open(filename,'w') as f: 
         json.dump(data, f, indent=4)
+    print("Written to JSON")
 
 with open('sheetslink.json') as json_file: 
     sheetslink = json.load(json_file) 
+    
 SCOPES_EMAIL = ['https://www.googleapis.com/auth/gmail.readonly']
 SCOPES_SHEET = ['https://www.googleapis.com/auth/drive']
 
@@ -76,6 +76,11 @@ def get_sheet_service():
     drive_service = googleapiclient.discovery.build('drive', 'v3', credentials=creds)
     return sheetservice, drive_service, emailservice
 
+def find_email(s):
+    x = s.split('<')
+    y = ''.join(x[1])
+    return y[0:len(y)-1]
+
 def main(service, user_id='me'):
     threads = service.users().threads().list(userId=user_id).execute().get('threads', [])
     cc_thread = []
@@ -93,18 +98,46 @@ def main(service, user_id='me'):
         if iscc:
 
             thread_id = tdata['id']
-            pprint(tdata['messages'][0]['payload']['headers'])
-            print('\n\n\n\n')
-            thread_client = re.search("\<(.*?)\>",tdata['messages'][0]['payload']['headers'][6]['value']).group(1)
-            thread_origin = re.search("\<(.*?)\>",tdata['messages'][0]['payload']['headers'][22]['value']).group(1)
+            for header in tdata['messages'][0]['payload']['headers']:
+                # pprint(header)
+                if header['name'] == "From":
+                    thread_client = re.search("\<(.*?)\>",header['value']).group(1)
+                elif header['name'] == "To":
+                    try:
+                        thread_origin = re.search("\<(.*?)\>",header['value']).group(1)
+                    except Exception as e:
+                        print(e)
+                        thread_origin = header['value']
+            
             print(thread_client)
             print(thread_origin)
+            spreadsheet_body = {
+                'properties': {
+                    'title': 'Your Email Chain w/ {email}'.format(email=thread_origin)
+                }}
             if thread_client not in sheetslink:
+                
                 temp = {thread_client:{
+                                thread_origin:{
+                                "sheetid": 0,
+                                "idlist": []
+                                }
+                            }
+                        }
+                sheetslink.update(temp)
+                spreadsheet = sheetservice.spreadsheets().create(body=spreadsheet_body,fields='spreadsheetId').execute()
+                sheetslink[thread_client][thread_origin]["sheetid"] = spreadsheet.get('spreadsheetId')
+            if thread_origin not in sheetslink[thread_client]:
+                temp = {
+                            thread_origin:{
                             "sheetid": 0,
                             "idlist": []
-                            }}
-                sheetslink.update(temp)
+                            }
+                        }
+                sheetslink[thread_client].update(temp)
+                spreadsheet = sheetservice.spreadsheets().create(body=spreadsheet_body,fields='spreadsheetId').execute()
+                sheetslink[thread_client][thread_origin]["sheetid"] = spreadsheet.get('spreadsheetId')
+            
             msg_body = tdata['messages'][0]['payload']['parts'][0]['body']['data']
             msg_raw = base64.urlsafe_b64decode(msg_body.encode('ASCII'))
             msg_str = email.message_from_bytes(msg_raw)
@@ -113,28 +146,32 @@ def main(service, user_id='me'):
                 #part 1 is plain text, part 2 is html
                 part1, part2 = msg_str.get_payload()
                 thread_response = find_message(part1.get_payload())
-                pprint(thread_response)
+                # pprint(thread_response)
             else:
                 thread_response = find_message(msg_str.get_payload())
-                pprint(thread_response)
+                # pprint(thread_response)
             
-            if thread_id not in sheetslink[thread_client]["idlist"]:
-                sheetslink[thread_client]["idlist"].append(thread_id)
-                if sheetslink[thread_client]["sheetid"] == 0:
-                    spreadsheet = sheetservice.spreadsheets().create(body=spreadsheet_body,fields='spreadsheetId').execute()
-                    sheetslink[thread_client]["sheetid"] = spreadsheet.get('spreadsheetId')
-                request = sheetservice.spreadsheets().values().get(spreadsheetId=sheetslink[thread_client]["sheetid"], range='A:Z', valueRenderOption='FORMATTED_VALUE', dateTimeRenderOption='SERIAL_NUMBER')
-                response = request.execute()
+            if thread_id not in sheetslink[thread_client][thread_origin]["idlist"]:
                 
-                values = response['values']
-                val_toadd = [thread_id,thread_origin,thread_client,thread_response]
-                values.append(val_toadd)
+                print("Adding New Email to Sheet")
+                sheetslink[thread_client][thread_origin]["idlist"].append(thread_id)
+                
+                request = sheetservice.spreadsheets().values().get(spreadsheetId=sheetslink[thread_client][thread_origin]["sheetid"], range='A:Z', valueRenderOption='FORMATTED_VALUE', dateTimeRenderOption='SERIAL_NUMBER')
+                response = request.execute()
+                pprint(response)
+                try:
+                    values = response['values']
+                    val_toadd = [thread_id,thread_origin,thread_client,thread_response]
+                    values.append(val_toadd)
+                except KeyError:
+                    values = [[thread_id,thread_origin,thread_client,thread_response]]
+                
                 pprint(values)
                 body = {
                 'values': values
                 }
-                result = sheetservice.spreadsheets().values().update(spreadsheetId=sheetslink[thread_client]["sheetid"],body=body, range='A:Z',valueInputOption='USER_ENTERED').execute()
-                share(drive_service, thread_client,sheetslink[thread_client]["sheetid"])
+                result = sheetservice.spreadsheets().values().update(spreadsheetId=sheetslink[thread_client][thread_origin]["sheetid"],body=body, range='A:Z',valueInputOption='USER_ENTERED').execute()
+                share(drive_service, thread_client,sheetslink[thread_client][thread_origin]["sheetid"])
 
 def share(drive_service,email,id):
     batch = drive_service.new_batch_http_request(callback=callback)
@@ -158,10 +195,13 @@ if __name__ == "__main__":
     try:
         sheetservice, drive_service, emailservice = get_sheet_service()
         while 1:
-            sleep(1)
             main(emailservice)
-            break
+            sleep(5)
+            # break
         write_json(sheetslink)
     except KeyboardInterrupt:
-        write_json(sheetslink)  
-        print("Program Ended.")
+        write_json(sheetslink)
+        print("Program Ended with a Keyboard Interrupt")
+    except Exception as e:
+        write_json(sheetslink)
+        print("Program Ended with an exception of "+ str(e))
