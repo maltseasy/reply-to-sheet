45cc6d2aad679cf6758420e28fc7141f8b6ecc07
Aryan Misra <aryanmisra4@gmail.com>
2020-04-14 00:21:29 -0400

    new emailer

--- emailer_new.py
+++ emailer_new.py
@@ -95,46 +95,46 @@ def main(service, user_id='me'):
             thread_id = tdata['id']
             pprint(tdata['messages'][0]['payload']['headers'])
             print('\n\n\n\n')
-            # thread_client = re.search("\<(.*?)\>",tdata['messages'][0]['payload']['headers'][6]['value']).group(1)
-            # thread_origin = re.search("\<(.*?)\>",tdata['messages'][0]['payload']['headers'][22]['value']).group(1)
-            # print(thread_client)
-            # print(thread_origin)
-            # if thread_client not in sheetslink:
-            #     temp = {thread_client:{
-            #                 "sheetid": 0,
-            #                 "idlist": []
-            #                 }}
-            #     sheetslink.update(temp)
-            # msg_body = tdata['messages'][0]['payload']['parts'][0]['body']['data']
-            # msg_raw = base64.urlsafe_b64decode(msg_body.encode('ASCII'))
-            # msg_str = email.message_from_bytes(msg_raw)
-            # content_type = msg_str.get_content_maintype()
-            # if content_type == 'multipart':
-            #     #part 1 is plain text, part 2 is html
-            #     part1, part2 = msg_str.get_payload()
-            #     thread_response = find_message(part1.get_payload())
-            #     pprint(thread_response)
-            # else:
-            #     thread_response = find_message(msg_str.get_payload())
-            #     pprint(thread_response)
+            thread_client = re.search("\<(.*?)\>",tdata['messages'][0]['payload']['headers'][6]['value']).group(1)
+            thread_origin = re.search("\<(.*?)\>",tdata['messages'][0]['payload']['headers'][22]['value']).group(1)
+            print(thread_client)
+            print(thread_origin)
+            if thread_client not in sheetslink:
+                temp = {thread_client:{
+                            "sheetid": 0,
+                            "idlist": []
+                            }}
+                sheetslink.update(temp)
+            msg_body = tdata['messages'][0]['payload']['parts'][0]['body']['data']
+            msg_raw = base64.urlsafe_b64decode(msg_body.encode('ASCII'))
+            msg_str = email.message_from_bytes(msg_raw)
+            content_type = msg_str.get_content_maintype()
+            if content_type == 'multipart':
+                #part 1 is plain text, part 2 is html
+                part1, part2 = msg_str.get_payload()
+                thread_response = find_message(part1.get_payload())
+                pprint(thread_response)
+            else:
+                thread_response = find_message(msg_str.get_payload())
+                pprint(thread_response)
             
-            # if thread_id not in sheetslink[thread_client]["idlist"]:
-            #     sheetslink[thread_client]["idlist"].append(thread_id)
-            #     if sheetslink[thread_client]["sheetid"] == 0:
-            #         spreadsheet = sheetservice.spreadsheets().create(body=spreadsheet_body,fields='spreadsheetId').execute()
-            #         sheetslink[thread_client]["sheetid"] = spreadsheet.get('spreadsheetId')
-            #     request = sheetservice.spreadsheets().values().get(spreadsheetId=sheetslink[thread_client]["sheetid"], range='A:Z', valueRenderOption='FORMATTED_VALUE', dateTimeRenderOption='SERIAL_NUMBER')
-            #     response = request.execute()
+            if thread_id not in sheetslink[thread_client]["idlist"]:
+                sheetslink[thread_client]["idlist"].append(thread_id)
+                if sheetslink[thread_client]["sheetid"] == 0:
+                    spreadsheet = sheetservice.spreadsheets().create(body=spreadsheet_body,fields='spreadsheetId').execute()
+                    sheetslink[thread_client]["sheetid"] = spreadsheet.get('spreadsheetId')
+                request = sheetservice.spreadsheets().values().get(spreadsheetId=sheetslink[thread_client]["sheetid"], range='A:Z', valueRenderOption='FORMATTED_VALUE', dateTimeRenderOption='SERIAL_NUMBER')
+                response = request.execute()
                 
-            #     values = response['values']
-            #     val_toadd = [thread_id,thread_origin,thread_client,thread_response]
-            #     values.append(val_toadd)
-            #     pprint(values)
-            #     body = {
-            #     'values': values
-            #     }
-            #     result = sheetservice.spreadsheets().values().update(spreadsheetId=sheetslink[thread_client]["sheetid"],body=body, range='A:Z',valueInputOption='USER_ENTERED').execute()
-            #     share(drive_service, thread_client,sheetslink[thread_client]["sheetid"])
+                values = response['values']
+                val_toadd = [thread_id,thread_origin,thread_client,thread_response]
+                values.append(val_toadd)
+                pprint(values)
+                body = {
+                'values': values
+                }
+                result = sheetservice.spreadsheets().values().update(spreadsheetId=sheetslink[thread_client]["sheetid"],body=body, range='A:Z',valueInputOption='USER_ENTERED').execute()
+                share(drive_service, thread_client,sheetslink[thread_client]["sheetid"])
 
 def share(drive_service,email,id):
     batch = drive_service.new_batch_http_request(callback=callback)
